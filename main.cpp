a7cd5475bf441d31d5f1f82a60e16074f9378589
Fish <thehuiq2006@gmail.com>
2026-09-21 16:33:12 -0700

    feat: image views

--- main.cpp
+++ main.cpp
@@ -76,6 +76,7 @@ class EngineApplication
 
 		VkSwapchainKHR swapChain;
 		std::vector<VkImage> swapChainImages;
+		std::vector<VkImageView> swapChainImageViews;
 		VkFormat swapChainImageFormat;
 		VkExtent2D swapChainExtent;
 
@@ -92,6 +93,35 @@ class EngineApplication
 			PickPhysicalDevice();
 			CreateLogicalDevice();
 			CreateSwapChain();
+			CreateImageViews();
+		}
+
+		void CreateImageViews()
+		{
+			swapChainImageViews.resize(swapChainImages.size());
+			for (size_t I = 0; I < swapChainImages.size(); I++)
+			{
+				VkImageViewCreateInfo createInfo{};
+				createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+				createInfo.image = swapChainImages[I];
+
+				createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+				createInfo.format = swapChainImageFormat;
+
+				createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+				createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+				createInfo.subresourceRange.baseMipLevel = 0;
+				createInfo.subresourceRange.levelCount = 1;
+				createInfo.subresourceRange.baseArrayLayer = 0;
+				createInfo.subresourceRange.layerCount = 1;
+
+				if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[I]) != VK_SUCCESS)
+					throw std::runtime_error("failed to create image views!");
+			}
 		}
 
 		void CreateSwapChain()
@@ -437,6 +467,10 @@ class EngineApplication
 
 		void Cleanup()
 		{
+			for (auto imageView : swapChainImageViews)
+			{
+				vkDestroyImageView(device, imageView, nullptr);
+			}
 			if(enableValidationLayers)
 			{
 				DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
