591f30a228e04dd4743fa30718cc8e6f069b5a0d
Fish <thehuiq2006@gmail.com>
2026-09-22 15:01:18 -0700

    wip: shader module

--- main.cpp
+++ main.cpp
@@ -15,6 +15,25 @@
 #include <cstdint>
 #include <limits>
 #include <algorithm>
+#include <fstream>
+
+static std::vector<char> ReadFile(const std::string& filename)
+{
+	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+	if (!file.is_open())
+		throw std::runtime_error("failed to open file!");
+
+	size_t fileSize = (size_t) file.tellg();
+	std::vector<char> buffer(fileSize);
+
+	file.seekg(0);
+	file.read(buffer.data(), fileSize);
+
+	file.close();
+
+	return buffer;
+}
 
 
 struct SwapChainSupportDetails
@@ -94,6 +113,34 @@ class EngineApplication
 			CreateLogicalDevice();
 			CreateSwapChain();
 			CreateImageViews();
+			CreateGraphicsPipeline();
+		}
+
+		void CreateGraphicsPipeline()
+		{
+			auto vertShaderCode = ReadFile("Shaders/Vert.spv");
+			auto fragShaderCode = ReadFile("Shaders/Frag.spv");
+
+			VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+			VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+			vkDestroyShaderModule(device, fragShaderModule, nullptr);
+			vkDestroyShaderModule(device, vertShaderModule, nullptr);
+		}
+
+		VkShaderModule createShaderModule(const std::vector<char>& code)
+		{
+			VkShaderModuleCreateInfo createInfo{};
+			createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+			createInfo.codeSize = code.size();
+			createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+			VkShaderModule shaderModule;
+			if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS)
+			{
+				throw std::runtime_error("failed to create shader module!");
+			}
+			return shaderModule;
 		}
 
 		void CreateImageViews()
